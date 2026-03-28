e1e6b8863623b69101b418453dc81362a884cf35
tchiwara <tinotendachiwara47@gmail.com>
2026-03-28 22:14:55 +0530

    feat: implement add() in TaskManager

--- src/main/java/com/tinotenda/task_tracker/TaskManager.java
+++ src/main/java/com/tinotenda/task_tracker/TaskManager.java
@@ -1,4 +1,26 @@
 package com.tinotenda.task_tracker;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class TaskManager {
+    FileManager fileManager=new FileManager();
+
+    public void add(String description) {
+        // read existing tasks
+        List<Task> tasks = fileManager.readTasks();
+
+        // generate next id
+        int newId = tasks.isEmpty() ? 1 : tasks.get(tasks.size() - 1).getId() + 1;
+
+        // create new task
+        Task task = new Task(newId, description, "todo", LocalDateTime.now(), LocalDateTime.now());
+
+        // add to list and save
+        tasks.add(task);
+        fileManager.writeTasks(tasks);
+
+        System.out.println("Task added successfully (ID: " + newId + ")");
+    }
+
 }
